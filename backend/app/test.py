a985e2b18cbe6d14903e69a8ed512acf6b584775
Ethan Luo <ethanluo14@gmail.com>
2026-02-17 12:54:46 -0500

    Remove data from repository and changed repo structure for Docker

--- backend/app/test.py
+++ backend/app/test.py
@@ -1,4 +1,4 @@
-from backend.pdf_utils import extract_text_by_page
+from backend.app.pdf_utils import extract_text_by_page
 
 pages = extract_text_by_page("backend/data/textbook.pdf")
 print(f"Total pages extracted: {len(pages)}")
